b2dd2c38577ce84da2689a1ac38259c0fc7a9d1c
Nam <lehoainam1998@gmail.com>
2022-03-14 15:15:50 +0700

    init project

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Not Found!</h1>
+      <Link to='/'>Go Home</Link>
+    </div>
+  );
+}
+
+export default NotFound;
