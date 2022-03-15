dc6da4c63a7d0535572cdfdde45084d143e0ff65
Nam <lehoainam1998@gmail.com>
2022-03-15 08:00:16 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='photo-app'>
+      <Header />
+      <Banner />
+      <ul>
+        <li>
+          <Link to='/'>Go to Home</Link>
+        </li>
+        <li>
+          <Link to='/photos'>Go to Photo</Link>
+        </li>
+        <li>
+          <Link to='photos/add'>Go to Add new image</Link>
+        </li>
+        <li>
+          <Link to='/photo/123'>Go to Edit photo page</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+import Loader from './components/Loader';
+import NotFound from './components/NotFound';
+import AddEditPage from './features/Photo/pages/AddEdit';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+const Photo = lazy(() => import('./features/Photo'));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/' element={<App />}>
+              <Route path='photos' element={<Photo />}>
+                <Route path='add' element={<AddEditPage />} />
+                <Route path='add/:photoId' element={<AddEditPage />} />
+              </Route>
+              <Route path='*' element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
